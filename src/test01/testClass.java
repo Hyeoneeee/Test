cf41222443a2d71105a7f7fe524bfba537fadf7b
tjsgu <tjsgu@DESKTOP-6OF3A4D>
2022-03-18 12:59:56 +0900

    This is commit message

---
+++ src/test01/testClass.java
@@ -0,0 +1,17 @@
+package test01;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// 테스트 코드 작성 후, 출력 결과 제출
+		
+		int x = 3;
+		int y = 5;
+		
+		System.out.println("x + y = " + (x+y) );
+		
+
+	}
+
+}
+
